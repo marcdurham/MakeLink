52c9cc574dee1ed5475819f85e852800b7e40b60
Marc Durham <marcdurham@gmail.com>
2017-09-21 01:32:46 +0800

    Add sample registry editing code

--- MakeLink/Program.cs
+++ MakeLink/Program.cs
@@ -1,4 +1,5 @@
 ﻿using IWshRuntimeLibrary;
+using Microsoft.Win32;
 
 namespace MakeLink
 {
@@ -7,14 +8,20 @@ namespace MakeLink
         static void Main(string[] args)
         {
             var shortcut = new IWshShell_Class().CreateShortcut(
-                @"C:\Users\marcd\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\LoopCAD.lnk"
-                ) as IWshShortcut;
+                @"E:\LoopCAD\LoopCAD.lnk") as IWshShortcut;
 
             shortcut.TargetPath = @"C:\Program Files (x86)\ProgeCAD\progeCAD 2014 Professional ENG\icad.exe";
             shortcut.IconLocation = @"E:\LoopCAD\LoopCAD.ico";
             shortcut.Arguments = @"/b E:\LoopCAD\LoopCAD.scr";
             shortcut.WorkingDirectory = @"E:\LoopCAD";
             shortcut.Save();
+
+            RegistryKey myKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Company\\Compfolder", true);
+            if (myKey != null)
+            {
+                myKey.SetValue("Deno", "1", RegistryValueKind.String);
+                myKey.Close();
+            }
         }
     }
 }
